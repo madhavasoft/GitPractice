83d0bcf77cc9be0347af7d9d38c24144eb17bcf5
Developer <Dev2@gmail.com>
2024-01-31 20:41:27 +0530

    4th Commit

--- GitTestingProject/src/org/customer/UtilityCust.java
+++ GitTestingProject/src/org/customer/UtilityCust.java
@@ -5,6 +5,7 @@ public class UtilityCust {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Test22");
+System.out.println("Test4 the commit");
 	}
 
 }
