eac71309885d9d315928b5ba701f195bc02df4df
New <New@gmail.com>
2024-02-01 11:00:32 +0530

    Revert "3rd prnt stmt"
    This reverts commit 83dcdb1fa8f7cdcb9750bc3624f47a0ce6c1742b.

--- GitTestingProject/src/org/customer/UtilityCust.java
+++ GitTestingProject/src/org/customer/UtilityCust.java
@@ -6,7 +6,6 @@ public class UtilityCust {
 		// TODO Auto-generated method stub
 System.out.println("Test22");
 System.out.println("Test4 the commit");
-System.out.println("This is 3rd stamt");
 	}
 
 }
